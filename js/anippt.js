2c78e9af9c5d9014dfdc2fd66fdf2c1dc449cf53
zhouzhichuang <zhouzhichuang@meizu.com>
2016-04-21 21:01:51 +0800

    tech complete

--- js/anippt.js
+++ js/anippt.js
@@ -1,12 +1,13 @@
 var winWidth = $(window).width() * 2;
 var winHeight = $(window).height() * 2;
-
 var transformData = {
 	'data-x': 0,
 	'data-y': 0,
 	'data-z': 0,
 	'data-rotate-x': 0,
 	'data-rotate-y': 0,
+	'data-rotate-z': 0,
+	//'data-rotate': 0,
 	'data-scale': 1,
 };
 
@@ -18,13 +19,37 @@ function sign() {
 $('.step').each(function() {
 	offset['data-x'] = sign() * winWidth;
 	offset['data-y'] = sign() * winHeight;
-	offset['data-rotate-x'] = sign() * parseInt(Math.random()*2) * 90;
-	offset['data-rotate-y'] = sign() * parseInt(Math.random()*2) * 90;
-	offset['data-rotate-z'] = sign() * parseInt(Math.random()*2) * 90;
+	offset['data-rotate-x'] = sign() * /*parseInt*/(Math.random()*2) * 90;
+	offset['data-rotate-y'] = sign() * /*parseInt*/(Math.random()*2) * 90;
+	offset['data-rotate-z'] = sign() * /*parseInt*/(Math.random()*2) * 90;
+	//offset['data-rotate'] = sign() * /*parseInt*/(Math.random()*2) * 90;
 
 	for (key in transformData) {
 		transformData[key] += offset[key];
 		$(this).attr(key, transformData[key]);
 	}
+
+	// add menu
+	if (this.id) {
+		$('.menu ul').append('<li><a href="#'+this.id+'">'+this.id+'</a></li>');
+	}
+});
+
+
+// menu 
+$(document).on('mousemove', function(e) {
+	if (e.clientX < 100 && e.clientY < 100) {
+		$('body').addClass('menu-show');
+	}
+});
+$('.menu').hover(function() {}, function() {
+	$('body').removeClass('menu-show');
+});
+
+$('code.html').each(function() {
+	var htmlStr = $(this).html();
+	htmlStr = htmlStr.replace(/\</g, '&lt;');
+	htmlStr = htmlStr.replace(/\>/g, '&gt;');
+	$(this).html(htmlStr);
 });
 impress().init();
